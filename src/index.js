182b6f7ca1fd911fac6d810a53595b049eb600b4
hassoeni <hassoeni_15@hotmail.com>
2022-10-21 14:22:42 +0200

    set up server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server-cloud-functions');
+const {
+  ApolloServerPluginLandingPageLocalDefault
+} = require('apollo-server-core');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-async function startApolloServer(typeDefs, resolvers) {
+// async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true, 
+    csrfPrevention: true, 
+    cache: 'bounded', 
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+],
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
@@ -14,12 +22,14 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
-  console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
-}
+//   const { url, port } = await server.listen();
+//   console.log(`
+//       🚀  Server is running
+//       🔉  Listening on port ${port}
+//       📭  Query at ${url}
+//     `);
+// // }
 
-startApolloServer(typeDefs, resolvers);
+
+exports.handler = server.createHandler(typeDefs, resolvers)
+// startApolloServer(typeDefs, resolvers);
